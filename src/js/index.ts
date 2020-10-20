1020fc4ea21a1ac9d3664ba189b0ae9e0fd99722
Nikolai Køhler Lundberg Hansen <niko9946@edu.easj.dk>
2020-10-20 13:30:34 +0200

    Nikolaj Pusher

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,8 +1,37 @@
+//Kald på rest
+let baseUrl: string = "http://recordsrest.azurewebsites.net/api/records"
+
+// import axios, {
+//     AxiosRespone,
+//     AxiosError
+// } from "../../node_modules/axios/index"
+
+interface IRecords{
+    id: number,
+    title: string,
+    artist: string,
+    yop: number,
+    albumRating: number
+}
+
+
+
+
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
+
+
+
+
+
+
+
+
+    
     data: {
         name: "",
         greeting: ""
